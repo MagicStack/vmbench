bc0f5d44961a7b3ce1e8abd9d7970eb589050c08
Yury Selivanov <yury@magic.io>
2016-04-27 11:51:41 -0400

    Add curio-streams server

---
+++ servers/curioecho_streams.py
@@ -0,0 +1,18 @@
+from curio import Kernel, new_task, run_server
+
+
+async def echo_handler(client, addr):
+    print('Connection from', addr)
+    reader, writer = client.make_streams()
+    async with reader, writer:
+        while True:
+            data = await reader.read()
+            if not data:
+                break
+            await writer.write(data)
+    await client.close()
+    print('Connection closed')
+
+if __name__ == '__main__':
+    kernel = Kernel()
+    kernel.run(run_server('', 25000, echo_handler))
